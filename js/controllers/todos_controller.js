cf0d9251fc31c6b2f5d89f6071bd1a5d021ce730
Michael Wiss <michael.wiss@gmail.com>
2015-08-14 17:45:41 -0500

    a confusing amount of scripts

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,6 +1,33 @@
+
+
+
+Todos.TodoController = Ember.ObjectController.extend({
+  actions: {
+    editTodo: function () {
+      this.set('isEditing', true);
+    }
+  },
+
+  isEditing: false,
+
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as  setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
+
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
-    createTodo: function() {
+    createTodo: function () {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
       if (!title.trim()) { return; }
@@ -17,5 +44,14 @@ Todos.TodosController = Ember.ArrayController.extend({
       // Save the new model
       todo.save();
     }
-  }
+  },
+
+  remaining: function () {
+    return this.filterProperty('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function () {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
 });
